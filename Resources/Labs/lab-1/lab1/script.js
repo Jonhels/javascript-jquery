28b0693ce9dcd07339b48239001f3dda8fd57e27
jonhels <jonhels8888@gmail.com>
2023-02-13 10:52:26 +0100

    goals

---
+++ Resources/Labs/lab-1/lab1/script.js
@@ -0,0 +1,53 @@
+/*  
+    How do I console.log a variable?
+    Use: console.log(variableName);
+*/
+
+// ******** Exercise 1 ******** 
+// a)
+
+
+// b)
+
+
+// c)
+
+
+// d)
+
+
+// ******** Exercise 2 ******** 
+// a)
+
+
+// b) 
+
+
+// ******** Exercise 3 ******** 
+// a)
+
+
+// b)
+
+
+// ******** Exercise 4 ******** 
+// a)
+let num1;
+let num2;
+let sum;
+
+// b) 
+let number1;
+let number2;
+let result;
+
+// c) 
+let x = 10;
+let y = 5;
+// Write the assignment operator:
+
+
+// d) 
+
+
+// e)
---
+++ readme.txt
@@ -0,0 +1,5 @@
+How to do the course: 
+- Read the powerpoints and do exercises on it's own html page
+- Read the book
+- Do the chapter exercises and create a website out of it
+- Add all course information into same repo in a structured fashion
