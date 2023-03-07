e4f8848a81d4e7299aaae1c1c1086e95116c597f
Jonhels <49123934+Jonhels@users.noreply.github.com>
2023-03-07 12:44:22 +0100

    Add files via upload
    manual commit wsl failed to load, saving backups

--- test2.js
+++ test2.js
@@ -113,3 +113,117 @@ console.log(newResult);
 // Order of operation - B I D M A S
 let formulaResult = 5 * (10 - 3) ** 2;
 console.log(formulaResult);
+
+// Different ways of adding value to an existing value
+let number = 10;
+
+// The value will become 11 after we add 1 to the current value 10
+number = number + 1;
+
+// This will do the same, adding 1 to the current value
+// This is the preffered way
+number++;
+
+// Subtract one fron the current value in number
+number--;
+
+// To add 10 to the current value
+number += 10;
+
+// To remove 5 from the current value
+number -= 5;
+
+// To multiply the current value by 2
+number *= 2;
+
+// To divide the current value by 2
+number /= 2;
+
+// These are called shorthand notation
+// +=, -=, *=, /=
+
+// NaN = Not a number
+
+// Number cincatenation
+let likes = 'the blog has ' + likes + ' likes';
+console.log(likes);
+
+// Template strings
+// Means that we inject variables into the string without needing to use + to concatenate variables
+
+const title = 'Best reads of 2023';
+const author = 'Mario';
+const favorites = 30;
+
+// concatenation way
+let concatenation = 'The blog called ' + title + ' by ' + author + ' has ' + favorites + ' favorites';
+console.log(concatenation);
+
+// Template string way
+let templateString = `The blog called ${title} by ${author} has ${favorites} favorites`;
+console.log(templateString);
+
+// Creating html templates
+let html = `
+    <h2>${title}</h2>
+    <p>${author}</p>
+    <span>This blog has ${favorites} favorites</span>
+`;
+console.log(html);
+
+// Arrays (object datatypes)
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+console.log(ninjas);
+
+// Get first item in the array
+console.log(ninjas[0]);
+
+// Change array item
+ninjas[1] = 'ken';
+console.log(ninjas[1]);
+
+// Strings and numbers in same array
+let combinedArray = ['shaun', 'crystal', 30, 20];
+console.log(combinedArray);
+
+// .length shows how many items the array have and outputs with a number
+console.log(combinedArray.length);
+
+// Array methods
+// Method in javascript means: acrions that can be performed on objects
+
+// .Join(), adds specified character between array abjects
+// We will add a comma
+let join = ninjas.join(',');
+console.log(join);
+
+// .indexOf(), returns the position of the first occurrence of a value in a string
+// Returns -1 if the value is not found
+let indexOf = ninjas.indexOf('chun-li');
+console.log(indexOf);
+
+// .concat(), merges two or more arrays
+let concat = ninjas.concat(['ken', 'crystal']);
+console.log(concat);
+
+// .push(), adds one or more elements to the end of an array and returns the new length of the array
+let push = ninjas.push('ken');
+console.log(push);
+
+// .pop(), removes the last element from an array and returns that value to the caller
+let pop = ninjas.pop();
+console.log(pop);
+
+// Null and undefined
+// Null value represents the intentinal avsence of any object value
+// Undefined means that there have not been assigned a value
+
+// Null example
+let aging = null;
+console.log(aging, aging + 3, `the age is ${aging}`);
+
+// Boolean
+// Booleans are the primitive data types that can either be true or false
+
+// Booleans $ comparisons
+console.log(true, false, 'true', 'false');
